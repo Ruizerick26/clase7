43ce1564e26838a89fb504d459be725aefc1ed01
Ruizerick26 <ruizerick2525@gmail.com>
2023-11-27 15:36:40 -0500

    seridores

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/*const http = require('node:http');
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
+
+server.listen(8000);*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
